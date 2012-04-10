10b2ecf8504183181cd867d33cc31fc886822f7c
Mikko Johansson <mikko.johansson@aalto.fi>
2012-04-10 17:25:42 +0300

    Initial layout done and map is working

---
+++ softGISview/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns
+from django.conf.urls import url
+
+urlpatterns = patterns('softGISview.views',
+    url(r'^$', 
+        'main',
+        name='front_page'),
+    url(r'^content/(?P<page_name>\w+)', 
+        'subcontent',
+        name='subcontent'),
+)
--- softGISview/views.py
+++ softGISview/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponse
+from django.http import HttpResponseBadRequest
+def main(request):
+    return render_to_response('welcome.html',
+                       context_instance=RequestContext(request))
+    
+def subcontent(request, page_name):
+    """
+    if request.is_ajax():
+        return HttpResponse('You requested: %s' % page_name)
+    else:
+        return HttpResponseBadRequest
+    """        
+    return HttpResponse('You requested: %s' % page_name)
