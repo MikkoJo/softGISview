092392a7ea18ea9eb07f5b954df8b6f739fe982b
Mikko Johansson <mikko.johansson@aalto.fi>
2012-07-06 14:11:47 +0300

    Added install script and test data to repo

---
+++ softGISview/install_full.sh
@@ -0,0 +1,22 @@
+#django_geonition_utils
+easy_install https://github.com/geonition/django_geonition_utils/tarball/2.0.0
+#django_auth
+easy_install https://github.com/geonition/django_auth/tarball/2.0.x
+#django_email
+#easy_install https://github.com/geonition/django_email/tarball/2.0.x
+#django_geojson_rest
+easy_install https://github.com/geonition/django_geojson_rest/tarball/2.0.x
+#django_user_profile (This is deprecated, install only if necessary)
+#easy_install https://github.com/geonition/django_user_profile/tarball/master
+#django_opensocial_people
+easy_install https://github.com/geonition/django_opensocial_people/tarball/2.0.x
+#django_opensocial_mediaitems
+#easy_install https://github.com/geonition/django_opensocial_mediaitems/tarball/master
+#django_geonition_client
+easy_install https://github.com/geonition/django_geonition_client/tarball/2.0.x
+#data_processing
+#easy_install https://github.com/geonition/django_geojson_data_processing/tarball/1.0.x
+#geodjango maps
+easy_install https://github.com/geonition/geodjango-map-layers/tarball/4.0.0
+#base_page
+easy_install https://github.com/geonition/base_page/tarball/1.0.0
